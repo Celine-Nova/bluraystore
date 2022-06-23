a8109abde0b4e5c840451eb13db50aa8638c88df
Celine-Nova <cocotte243@gmail.com>
2022-06-23 16:52:22 +0200

    creer un template dynamique Thymeleaf

---
+++ src/main/java/com/mycompany/bluraystore/controller/AboutUsController.java
@@ -0,0 +1,13 @@
+package com.mycompany.bluraystore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AboutUsController {
+    @RequestMapping("/about")
+    public String displayAboutUs(){
+        System.out.println("La page about us a été affichée");
+        return "about-us";
+    }
+}
