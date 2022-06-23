fcdd9d7abc95853a1ec185492f4f139bd8ecadfb
Celine-Nova <cocotte243@gmail.com>
2022-06-23 14:34:16 +0200

    passage Spring Boot

--- src/main/java/com/mycompany/bluraystore/App.java
+++ src/main/java/com/mycompany/bluraystore/App.java
@@ -1,6 +1,8 @@
 package com.mycompany.bluraystore;
 
 import com.mycompany.bluraystore.controller.MovieController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,18 +14,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Hello world!
  *
  */
-@Configuration
-@ComponentScan(basePackages = {"com.mycompany.bluraystore.controller",
-        "com.mycompany.bluraystore.service",
-        "com.mycompany.bluraystore.repository.file"
-})
-@PropertySource("classpath:application.properties")
+//@Configuration
+//@ComponentScan(basePackages = {"com.mycompany.bluraystore.controller",
+//        "com.mycompany.bluraystore.service",
+//        "com.mycompany.bluraystore.repository.file"
+//})
+//@PropertySource("classpath:application.properties")
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = SpringApplication.run(App.class);
         MovieController controller = context.getBean(MovieController.class);
         controller.addUsingConsole();
 
